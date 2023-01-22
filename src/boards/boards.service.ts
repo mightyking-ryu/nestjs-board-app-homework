4ec8f03f62a7a9b46b7603d30f06fbaf1454180c
mightyking-ryu <dongwooryu@snu.ac.kr>
2023-01-22 12:34:36 +0900

    prisma without authentication

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,97 +1,46 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { BoardStatus } from './board-status.enum';
+import { Injectable } from '@nestjs/common';
+import { BoardStatus } from '@prisma/client';
 import { CreateBoardDto } from './dto/create-board.dto';
-import { BoardRepository } from './board.repository';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Board } from './board.entity';
-import { User } from 'src/auth/user.entity';
+import { Board } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class BoardsService {
-    constructor(
-        @InjectRepository(BoardRepository)
-        private boardRepository: BoardRepository,
-    ) { }
-    // getAllBoards(): Board[] {
-    //     return this.boards;
-    // }
+    constructor(private prismaService: PrismaService){}
 
-    async getAllBoards(
-        user: User
-    ): Promise<Board[]> {
-        const query = this.boardRepository.createQueryBuilder('board');
-
-        query.where('board.userId = :userId', { userId: user.id});
-
-        const boards = await query.getMany();
-
-        return boards;
+    async getAllBoards(): Promise<Board[]> {
+        return await this.prismaService.board.findMany();
     }
 
-    // createBoard(createBoardDto: CreateBoardDto) {
-    //     const { title, description } = createBoardDto;
+    async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+        const {title, description} = createBoardDto;
 
-    //     const board: Board = {
-    //         id: uuid(),
-    //         title,
-    //         description,
-    //         status: BoardStatus.PUBLIC
-    //     }
-
-    //     this.boards.push(board);
-    //     return board;
-    // }
-
-    createBoard(createBoardDto: CreateBoardDto, user: User): Promise<Board> {
-        return this.boardRepository.createBoard(createBoardDto, user);
-    }
-
-    async getBoardById(id: number): Promise<Board> {
-        const found = await this.boardRepository.findOne(id);
-
-        if (!found) {
-            throw new NotFoundException(`Can't find Board with id ${id}`);
-        }
-
-        return found;
-    }
-
-    // getBoardById(id: string): Board {
-    //     const found = this.boards.find((board) => board.id === id);
-
-    //     if (!found) {
-    //         throw new NotFoundException(`Can't find Board with id ${id}`);
-    //     }
-
-    //     return found;
-    // }
-
-    async deleteBoard(id: number, user: User): Promise<void> {
-        const result = await this.boardRepository.delete({id, user});
-
-        if (result.affected === 0) {
-            throw new NotFoundException(`Can't find Board with id ${id}`)
-        }
-    }
-
-    // deleteBoard(id: string): void {
-    //     const found = this.getBoardById(id);
-    //     this.boards = this.boards.filter((board) => board.id !== found.id);
-    // }
-
-
-    async updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
-        const board = await this.getBoardById(id);
-
-        board.status = status;
-        await this.boardRepository.save(board);
-
+        const board = await this.prismaService.board.create({
+            data:{
+                title,
+                description,
+                status: BoardStatus.PUBLIC
+            },
+        });
         return board;
     }
-    // updateBoardStatus(id: string, status: BoardStatus): Board {
-    //     const board = this.getBoardById(id);
-    //     board.status = status;
-    //     return board;
-    // }
 
+    async getBoardById(id: number): Promise <Board> {
+        return await this.prismaService.board.findUnique({
+            where: { id }
+        });
+    }
+
+    async deleteBoard(id: number): Promise<void> {
+        const result = await this.prismaService.board.delete({
+            where: { id }
+        });
+    }
+
+    async updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
+        return await this.prismaService.board.update({
+            where: { id },
+            data: { status }
+        });
+    }
 }
--- src/boards/pipes/board-status-validation.pipe.ts
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -1,13 +1,15 @@
-import { BadRequestException, PipeTransform } from "@nestjs/common";
-import { BoardStatus } from "../board-status.enum";
+import { BadRequestException } from "@nestjs/common";
+import { PipeTransform } from "@nestjs/common/interfaces";
+import { BoardStatus } from "@prisma/client";
 
 export class BoardStatusValidationPipe implements PipeTransform {
+
     readonly StatusOptions = [
         BoardStatus.PRIVATE,
         BoardStatus.PUBLIC
-    ]
+    ];
 
-    transform(value: any){
+    transform(value: any) {
         value = value.toUpperCase();
 
         if(!this.isStatusValid(value)) {
@@ -19,7 +21,6 @@ export class BoardStatusValidationPipe implements PipeTransform {
 
     private isStatusValid(status: any) {
         const index = this.StatusOptions.indexOf(status);
-        return index !== -1
+        return index !== -1;
     }
-
 }
